a9465e076b232636b61a57a7503d994e8b3bd30d
Parker <pp101socials@gmail.com>
2023-09-25 15:12:31 -0400

    Created file

--- utils/connectToDB.js
+++ utils/connectToDB.js
@@ -9,32 +9,26 @@ import { MongoClient } from "mongodb";
 const URI =
   "mongodb+srv://admin:client_reminders_DB_020700@clientreminders.18gqsln.mongodb.net/?retryWrites=true&w=majority";
 
+let cachedClient = null;
+
 export async function connectToDB() {
-  let cachedClient = null;
-  let cachedDb = null;
-
-  if (cachedDb) {
-    return cachedDb;
-  }
-
   if (!cachedClient) {
-    // Creating the connection
-    cachedClient = new MongoClient(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
     try {
+      // Creating the connection
+      const client = new MongoClient(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
       // Connecting
-      await cachedClient.connect();
+      await client.connect();
       console.log("Connected to MongoDB");
+      cachedClient = client; // Cache the client object
     } catch (error) {
       console.error("Error connecting to database: " + error);
       throw error;
     }
   }
 
-  cachedDb = cachedClient.db(); // Get the database object
-
-  return cachedDb;
+  return cachedClient.db(); // Get the database object
 }
